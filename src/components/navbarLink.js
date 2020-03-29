515556e2935204feed1835b5032c3e68dc3a8343
Your Name <you@example.com>
2020-03-29 19:29:23 +0300

    Finalising navbar menu

--- src/components/navbarLink.js
+++ src/components/navbarLink.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef, createRef } from 'react'
 import "../styles/navbar.css"
 import { Link } from "react-router-dom";
 
-export default function NavbarLink() {
-    return (<React.Fragment>
-        <div className="navbar">
-            <ul className="navLinks">
-                <li><Link className="link" to="/cards">Presenters</Link></li>
-                <li><Link className="link" to="/review">Reviews</Link></li>
-                <li><Link className="link" to="/">Book us</Link></li>
-            </ul>
-        </div>
-    </React.Fragment>)
+class NavbarLink extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            navbar: React.createRef(),
+            menubtn: React.createRef()
+        }
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+    closeMenu() {
+        this.state.navbar.current.style.height = "0";
+    }
+    render() {
+        return (<React.Fragment>
+            <span className="menu-icon" onClick={() => {
+                this.state.navbar.current.style.height = "100%"
+            }}>&#9776;</span>
+            <div ref={this.state.navbar} className="menu-container">
+                <span className="closeBtn" onClick={this.closeMenu}>&times;</span>
+                <div className="menu-content">
+                    <Link onClick={this.closeMenu} to="/review" className="link">Reviews</Link>
+                    <Link onClick={this.closeMenu} to="cards" className="link">Presenters</Link>
+                    <Link onClick={this.closeMenu} to="#" className="link">Book us</Link>
+                </div>
+            </div>
+        </React.Fragment>)
+    }
 }
+export default NavbarLink;
